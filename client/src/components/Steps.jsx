9e46893d35e904aff49bac09321cb705152a343d
Reshoo-18 <reshooranjan7@gmail.com>
2025-11-05 22:47:26 +0530

    Steps added

---
+++ client/src/components/Steps.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Steps = () => {
+  return (
+    <div>
+      <h1 className='text-3xl sm:text-4xl font-semibold mb-2'>How its works</h1>
+      <p className='text-lg text-gray-500'>Transform Words Intro Stunning Images</p>
+    </div>
+  )
+}
+
+export default Steps
