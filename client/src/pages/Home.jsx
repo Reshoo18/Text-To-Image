2a01fca7491fd327afb65405c8e3b6baaa41ef54
Reshoo-18 <reshooranjan7@gmail.com>
2025-11-13 22:44:27 +0530

    home page added

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import Header from '../components/Header'
+import Steps from '../components/steps'
+import Description from '../components/description'
+import Testimonials from '../components/Testimonials'
 
 const Home = () => {
   return (
     <div>
      <Header/>
+     <Steps/>
+     <Description/>
+     <Testimonials/>
     </div>
   )
 }
