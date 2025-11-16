4ce3cc4cab41651727b7d45fe2c74ffc20b408f7
Reshoo-18 <reshooranjan7@gmail.com>
2025-11-16 23:09:20 +0530

    login oage

--- client/src/pages/Result.jsx
+++ client/src/pages/Result.jsx
@@ -6,12 +6,16 @@ const Result = () => {
    const [image,setImage]=useState(assets.sample_img_1)
    const [isImageLoaded,setImageLoaded]=useState(false)
    const [loading,setLoading]=useState(false)
+   const [input,setInput]=useState('')
+   const onSubmitHandler=async (e)=>{
+
+   }
   return (
-    <form className='flex flex-col min-h-[90vh] justify-center items-center'>
+    <form onSubmit={onSubmitHandler} className='flex flex-col min-h-[90vh] justify-center items-center'>
     <div>
       <div className='relative'>
         <img src={image} alt="" className='max-w-sm rounded'/>
-        <span className='absolute bottom-0 left-0 h-1 bg-blue-500 w-full transistion-all duration-[10s]'/> 
+        <span className={`absolute bottom-0 left-0 h-1 bg-blue-500 ${loading?'w-full transistion-all duration-[10s]' : 'w-0'}`}/> 
       </div>
 
       <p className={!loading ? 'hidden' : ''}>Loading....</p>
@@ -19,7 +23,9 @@ const Result = () => {
     </div>
     {!isImageLoaded && 
     <div className='flex w-full max-w-xl bg-neutral-500 text-white text-sm p-0.5 mt-10 rounded-full'>
-       <input type="text" placeholder='Describe what you want to generate ' className='flex-1 bg-transparent outline-none ml-8 max-sm:w-20 
+       <input 
+       onChange={e=> setInput(e.target.value)} value={input}
+       type="text" placeholder='Describe what you want to generate ' className='flex-1 bg-transparent outline-none ml-8 max-sm:w-20 
        placeholder-color' 
        />
        <button type='submit' className='bg-zinc-900 px-10 sm:px-16 py-3 rounded-full'>Generate</button>
